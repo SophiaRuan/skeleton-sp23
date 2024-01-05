18693161a03dee3861e23d2ff42b7e58952ab55b
SophiaRuan <xiaoxu_ruan@berkeley.edu>
2024-01-05 15:33:07 -0800

    hw0b

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,25 +1,54 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
 	public static int sum(List<Integer> L) {
-        return 0;
+        if (L.isEmpty()) {
+            return 0;
+        } else {
+            int result = 0;
+            for (int i : L) {
+                result += i;
+            }
+            return result;
+        }
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        for (int item : L) {
+            if (item % 2 == 0) {
+                result.add(item);
+            }
+        }
+        return result;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        for (int i=0; i < L1.size(); i++) {
+            if (L2.contains(L1.get(i))) {
+                result.add(L1.get(i));
+            }
+        }
+        return result;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        return 0;
+        int result = 0;
+        for (String w : words) {
+            for (int i=0; i <w.length(); i++) {
+                if (w.charAt(i)==c) {
+                    result++;
+                }
+            }
+        }
+        return result;
     }
 }
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,23 +1,41 @@
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        return null;
+        Map<Character, Integer> result = new TreeMap<>();
+        char c = 'a';
+        for (int i=1; i <= 26; i++, c++) {
+            result.put(c,i);
+        }
+        return result;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        return null;
+        Map<Integer, Integer> result = new TreeMap<>();
+        for (int i: nums) {
+            result.put(i,i*i);
+        }
+        return result;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        return null;
+        Map<String, Integer> result = new TreeMap<>();
+        for (String c : words) {
+            if (result.containsKey(c)) {
+                result.put(c,result.get(c)+1);
+            } else {
+                result.put(c,1);
+            }
+        }
+        return result;
     }
 }
